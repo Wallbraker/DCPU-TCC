33625f2cc8c14c92056065ca3ac3f708f6e7cb3c
Jakob Bornecrantz <wallbraker@gmail.com>
2012-04-10 02:04:51 +0200

    Add a ASM memcpy implementation

--- lib/crt0.c
+++ lib/crt0.c
@@ -49,3 +49,39 @@ void _start()
     main();
     dexit();
 }
+
+/*
+
+void memcpy(void *dst, const void *src, unsigned length)
+
+;
+; Swap SP and B around, allowing to read fast with [SP++]
+; and skipping a ADD B, 1, saving use cycles instructions.
+;
+	SET O, SP                            ; 0x0000 | 6dd1
+	SET SP, B                            ; 0x0001 | 05b1
+	SET B, O                             ; 0x0002 | 7411
+;
+; Calculate last address, by comparing addresses
+; instead we negate the need for a counter.
+;
+	ADD C, SP                            ; 0x0003 | 6c22
+	IFE SP, C                            ; 0x0004 | 09bc
+	ADD PC, 0x03                         ; 0x0005 | 8dc2
+	SET [A], [SP++]                      ; 0x0006 | 6081
+	ADD A, 0x01                          ; 0x0007 | 8402
+	SUB PC, 0x05                         ; 0x0008 | 95c3
+;
+; Restore SP and return.
+;
+	SET SP, B                            ; 0x0009 | 05b1
+	SET PC, [SP++]                       ; 0x000a | 61c1
+*/
+unsigned short memcpy[11] = {
+	0x6dd1, 0x05b1,
+	0x7411, 0x6c22,
+	0x09bc, 0x8dc2,
+	0x6081, 0x8402,
+	0x95c3, 0x05b1,
+	0x61c1,
+};
